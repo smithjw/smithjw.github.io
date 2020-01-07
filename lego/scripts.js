48c0b3cceb9a86180df6970fb904853400b3a905
James Smith <james@smithjw.me>
2020-01-07 22:50:23 +1100

    Working on persisting memory

--- lego/scripts.js
+++ lego/scripts.js
@@ -1,72 +1,108 @@
-const set = '7191-1';
+// const set = '7191-1';
 // const set = 'MOC-7540';
 const app = document.getElementById('root');
 const logo = document.createElement('img');
-var type = 'sets';
-
-let url = `https://rebrickable.com/api/v3/lego/${type}/${set}`;
-let parts_url = "";
-
-fetch(request(url))
-    .then(response => {
-
-        if (response.ok) {
-            parts_url = `${url}/parts/?page_size=100000`;
-            window.addEventListener('load', partList(parts_url));
-            return response;
-        } else {
-            type = 'mocs';
-            url = `https://rebrickable.com/api/v3/lego/${type}/${set}`;
-            console.log(url)
-            fetch(request(url))
-                .then(response => {
-                    return response.json();
-                })
-                .then(set => {
-                    console.log(set)
-                    logo.src = set.moc_img_url
-                    parts_url = `${url}/parts/?page_size=100000`;
-                    window.addEventListener('load', partList(parts_url));
-                })
-                .catch(console.error)
-            return response;
-        }
-
-    })
-    .then(response => response.json())
-    .then(set => {
-        console.log(set)
-        logo.src = set.set_img_url
-    })
-    .catch(console.error)
-
 const container = document.createElement('div');
+
+let setValue = localStorage.getItem('set') ? localStorage.getItem('set') : '';
+var key = '';
+
+logo.src = 'lego.svg'
+var type = 'sets';
+var url = `https://rebrickable.com/api/v3/lego/${type}/${setValue}`;
+var parts_url = "";
+
 container.setAttribute('class', 'container');
+logo.setAttribute('class', 'headerImage');
 
 var options = document.createElement('div');
-options.setAttribute('class', 'buttons');
 var clearStorage = document.createElement('button');
+var apiKey = document.createElement('input');
+var setID = document.createElement('input');
+var loadSet = document.createElement('button');
+
+options.setAttribute('class', 'buttons');
+apiKey.setAttribute('class', 'field-light text');
+apiKey.setAttribute('type', 'text');
+apiKey.setAttribute('id','apiKey');
+apiKey.setAttribute('placeholder','User API Key');
+setID.setAttribute('class', 'field-light text');
+setID.setAttribute('type', 'text');
+setID.setAttribute('id', 'set');
+setID.setAttribute('placeholder', 'LEGO Set ID');
+
 clearStorage.innerHTML = "Clear Storage";
 clearStorage.onclick = function () {
     localStorage.clear();
     window.location.reload();
 };
 
+loadSet.innerHTML = "Load";
+loadSet.onclick = function () {
+    set = document.getElementById('set').value;
+    checkSetID();
+};
+
+apiKey.onclick = function () {
+    apiKey.defaultValue = '';
+};
+
+setID.onclick = function () {
+    setID.defaultValue = '';
+};
+
 app.appendChild(logo);
 app.appendChild(options);
 options.appendChild(clearStorage);
+options.appendChild(apiKey);
+options.appendChild(setID);
+options.appendChild(loadSet);
 app.appendChild(container);
 
 function request(url) {
     var request = new Request(url, {
         method: 'GET',
         headers: new Headers({
-            'Authorization': 'key 9979ce0e366a5c09a4e8dbb6fd1e28f6',
+            'Authorization': 'key d72a0a731b4ae4b0baf67ad1cc2eb1c1',
             'Accept': 'application/json'
         })
     });
     return request;
 };
+
+function checkSetID() {
+    fetch(request(url))
+        .then(response => {
+            if (response.ok) {
+                parts_url = `${url}/parts/?page_size=100000`;
+                window.addEventListener('load', partList(parts_url));
+                return response;
+            }
+            else {
+                type = 'mocs';
+                url = `https://rebrickable.com/api/v3/lego/${type}/${setValue}`;
+                console.log(url);
+                fetch(request(url))
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(set => {
+                        console.log(set);
+                        logo.src = set.moc_img_url;
+                        parts_url = `${url}/parts/?page_size=100000`;
+                        window.addEventListener('load', partList(parts_url));
+                    })
+                    .catch(console.error);
+                return response;
+            }
+        })
+        .then(response => response.json())
+        .then(set => {
+            console.log(set);
+            logo.src = set.set_img_url;
+        })
+        .catch(console.error);
+}
 
 function partList(url) {
 
