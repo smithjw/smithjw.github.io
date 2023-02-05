4dc09179979dc6d1c32b8ff73d9098735b2f0f02
James Smith <james@smithjw.me>
2023-02-05 21:21:03 +1100

    Adding comments to redirect script

--- add_alias.sh
+++ add_alias.sh
@@ -2,17 +2,19 @@
 
 # Create aliases for old posts.
 
-DIR="content/writing"
+DIR="$1"
 
 for filename in "$DIR"/*.md; do
+    echo "Adding frontmatter to: $filename"
+
     basename=$(basename "$filename" ".md")
     year="${basename:0:4}"
     month="${basename:5:2}"
     day="${basename:8:2}"
     title="${basename:11}"
 
-    alias_block="aliases:\n  - /$year/$month/$day/$title"
-    echo "$alias_block"
-    sed -i '' "1s#---#---\n$alias_block#g" "$filename"
+    alias_block="/$year/$month/$day/$title"
+    echo "Adding alias from: $alias_block"
+    sed -i '' "1s#---#---\naliases:\n  - $alias_block#g" "$filename"
 
 done
